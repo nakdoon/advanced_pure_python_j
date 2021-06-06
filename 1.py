2f43c230b0d65e17b04f12d62270f966c0fbfb8b
= <emi.erian@gmail.com>
2021-06-06 21:27:57 +0430

    some changes

--- 1.py
+++ 1.py
@@ -1,9 +1,4 @@
-count = 1
-output = str()
-n = int(input ())
-while count <= n+1 :
-  for i in range(1,count):
-    output +=str(i)
-  print(output)
-  output = ''
-  count += 1
+import math
+import statistics
+a=7
+print(a)
